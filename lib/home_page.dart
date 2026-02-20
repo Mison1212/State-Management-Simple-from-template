4ccdbf606403794f6ed2d8d92d8222a7a3db861c
cendyal-hub <sendyaja17@gmail.com>
2026-02-20 22:55:31 +0700

    update file home_page.dart, rubah pada Builder menjadi Consumer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,26 +10,15 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final angka = context.watch<CounterModel>().angka;
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text('$angka', style: Theme.of(context).textTheme.headlineMedium),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => context.read<CounterModel>().increment(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return Consumer<CounterModel>(
+      builder: (context, counterModel, child) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(title),
+          ),
+        );
+      },
     );
   }
 }
