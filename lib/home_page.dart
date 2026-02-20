0f0c715e7b3d762e21c8d98baf48c50c3d5f6916
cendyal-hub <sendyaja17@gmail.com>
2026-02-20 22:31:19 +0700

    menghapus variabel _counter dan method _incrementCounter() pada Home page dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,7 +9,7 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final counter = context.watch<CounterModel>().angka;
+    final angka = context.watch<CounterModel>().angka;
 
     return Scaffold(
       appBar: AppBar(
@@ -21,7 +21,7 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text('$counter', style: Theme.of(context).textTheme.headlineMedium),
+            Text('$angka', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
