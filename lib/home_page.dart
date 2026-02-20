6ddcc1410bde1b092c4d163875fcbefc15510ece
cendyal-hub <sendyaja17@gmail.com>
2026-02-20 23:02:39 +0700

    update file home_page.dart, rubah pada Tampilan text counter

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,13 +9,29 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final angka = context.watch<CounterModel>().angka;
     return Consumer<CounterModel>(
       builder: (context, counterModel, child) {
         return Scaffold(
           appBar: AppBar(
             backgroundColor: Theme.of(context).colorScheme.inversePrimary,
             title: Text(title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('You have pushed the button this many times:'),
+                Text(
+                  counterModel.angka.toString(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => counterModel.increment(),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
           ),
         );
       },
